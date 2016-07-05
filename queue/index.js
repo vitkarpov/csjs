dd64f509a4f10603c6f8e7b7e9df383dec5865f3
vitkarpov <viktor.s.karpov@gmail.com>
2016-07-05 12:46:09 +0300

    fix priority queue

--- queue/index.js
+++ queue/index.js
@@ -1,41 +1,36 @@
 class Queue {
   constructor() {
-    this._store = {};
+    this._store = [];
   }
 
   enqueue(item, priority = 0) {
-    if (!this._store[priority]) {
-        this._store[priority] = [];
-    }
-    this._store[priority].push(item);
+    this._store.push({ item, priority });
 
     return this;
   }
 
   dequeue() {
-    const priorities = Object.keys(this._store).sort((a, b) => {
-        return a >= b;
-    });
-    let highest = priorities.pop();
-
-    while (highest) {
-        if (this._store[highest].length) {
-            return this._store[highest].shift();
+    const highest = this._store.reduce((memo, item, index) => {
+        if (item.priority < memo.priority) {
+            memo.priority = item.priority;
+            memo.index = index;
         }
-        highest = priorities.pop();
-    }
+        return memo;
+    }, { priority: 99, index: -1 });
+
+    return (this._store.splice(highest.index, 1)[0] || {}).item;
   }
 
   front() {
-    return this._store[0];
+    return this._store[0].item;
   }
 
   back() {
-    return this._store[this._store.length - 1];
+    return this._store[this._store.length - 1].item;
   }
 
   empty() {
-    return Boolean(this._store.length);
+    return this._store.length === 0;
   }
 
   toString() {
